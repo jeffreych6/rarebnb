5336a64c0f346268079daf7a1f398ab38a7a2e2b
Jeffrey Cheng <jeffreych6@gmail.com>
2023-01-07 22:12:51 -0500

    footer links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,10 +6,17 @@ function Footer() {
     <footer>
         <div className="footer-container">
             <div className="footer-left">
-              © 2023 RareBnB, Inc. · Terms · Sitemap · Privacy · Your Privacy Choices
+              © 2023 RareBnB, Inc. · React · Redux · JavaScript · Ruby · Rails · PostgreSQL
             </div>
             <div className="footer-right">
-                English
+                Designed by Jeffrey Cheng ·
+                <a href="/#"> Portfolio </a>
+                ·
+                <a href="https://github.com/jeffreych6"> GitHub </a>
+                ·
+                <a href="https://www.linkedin.com/in/jeffrey-cheng-38338290"> LinkedIn </a>
+                ·
+                <a href="https://angel.co/u/jeffrey-cheng-15"> Angel List</a>
             </div>
         </div>
     </footer>
