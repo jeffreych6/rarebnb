a8133c52b5764f13dd18b9064b1770b706fb0c51
Jeffrey Cheng <jeffreych6@gmail.com>
2023-02-12 10:36:38 -0500

    user show right outline

--- frontend/src/components/User/index.js
+++ frontend/src/components/User/index.js
@@ -22,40 +22,51 @@ function User() {
             <div className="user-show-page-container">
                 <div className="user-show-left-container">
                     <div className="user-show-left-box">
-                        <img className="user-show-profile"src={user.photoUrl}></img>
-                        <div>
-                            <i className="fa-solid fa-star"></i>
-                            Software Engineer
-                        </div>
-                        <div>
-                            <i className="fa-solid fa-star"></i>
-                            Software Engineer
-                        </div>
-                        <div>
-                            <i className="fa-solid fa-star"></i>
-                            Software Engineer
-                        </div>
-                        <div className="reservation-form-divider"></div>
-                        <div>
-                            <i class="fa-solid fa-check"></i>
-                            Identity
-                        </div>
-                        <div>
-                            <i class="fa-solid fa-check"></i>
-                            Email address
-                        </div>
-                        <div>
-                            <i class="fa-solid fa-check"></i>
-                            Phone number
-                        </div>
- 
 
+
+                            <img className="user-show-profile"src={user.photoUrl}></img>
+                            <div>
+                                <i className="fa-solid fa-star"></i>
+                                Software Engineer
+                            </div>
+                            <div>
+                                <i className="fa-solid fa-star"></i>
+                                Software Engineer
+                            </div>
+                            <div>
+                                <i className="fa-solid fa-star"></i>
+                                Software Engineer
+                            </div>
+                            <div className="reservation-form-divider"></div>
+                            <div>
+                                {user.firstName} confirmed
+                            </div>
+                            <div>
+                                <i class="fa-solid fa-check"></i>
+                                Identity
+                            </div>
+                            <div>
+                                <i class="fa-solid fa-check"></i>
+                                Email address
+                            </div>
+                            <div>
+                                <i class="fa-solid fa-check"></i>
+                                Phone number
+                            </div>
 
 
                     </div>
                 </div>
 
                 <div className="user-show-right-container">
+                    <h1 className="user-show-right-title">Hi, I'm {user.firstName}</h1>
+                    <div className="user-show-right-date">Joined in 2022</div>
+                    <div className="user-show-right-about">
+                        <h1>About</h1>
+                        <span className="user-show-right-description">hello, i'm jeffrey</span>
+                        <span className="user-show-right-bullets"><i className="fa-solid fa-star"></i>Lives in New York, NY</span>
+                        <span className="user-show-right-bullets"><i className="fa-solid fa-star"></i>Speaks English</span>
+                    </div>
 
                 </div>
             </div>
